2dd953c925cff76978a79ec6bde43bbe47c913e6
MaxKlimov1 <112946078+MaxKlimov1@users.noreply.github.com>
2023-07-05 12:05:31 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
-const burgerButton = document.querySelector('.header_burger-menu');
-const burgerMenu = document.querySelector('.burger_main-block');
-const closeBtn = document.querySelector('.close-btn');
+const burgerButton = document.querySelector('.header_burger-menu'),
+    burgerMenu = document.querySelector('.burger_main-block'),
+    closeBtn = document.querySelector('.close-btn');
+
+const arrowLeft = document.querySelector('.left-arrow'),
+    arrowRight = document.querySelector('.right-arrow'),
+    workerList = document.querySelectorAll('.main_workers-arr'),
+    workerItem = document.querySelectorAll('.main_worker-item')
 
 burgerButton.addEventListener('mouseup', e => {
     e.preventDefault();
@@ -10,4 +15,38 @@ burgerButton.addEventListener('mouseup', e => {
 closeBtn.addEventListener('mouseup', e => {
     e.preventDefault();
     burgerMenu.style.display = 'none';
+});
+
+let slyderCounter = 1;
+
+function slyderCardFun() {
+
+    if (slyderCounter > workerItem.length) {
+        slyderCounter = 1;
+    }
+
+    if (slyderCounter < 1) {
+        slyderCounter = workerItem.length;
+    }
+
+    workerItem.forEach(item => {
+        return item.style.display = "none"
+    });
+
+    workerItem[slyderCounter - 1].style.display = 'block';
+}
+
+slyderCardFun(slyderCounter);
+
+
+arrowLeft.addEventListener("click", (e) => {
+    e.preventDefault();
+    slyderCounter += 1;
+    slyderCardFun();
+})
+
+arrowRight.addEventListener("click", (e) => {
+    e.preventDefault();
+    slyderCounter -= 1;
+    slyderCardFun();
 })
